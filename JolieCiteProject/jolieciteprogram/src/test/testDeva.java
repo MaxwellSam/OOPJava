cb35a3e1d771356922661080fdf3cf6be2dd740b
deva-sou <soudeva974@gmail.com>
2021-12-09 11:39:50 +0100

    adding date to the project

---
+++ JolieCiteProject/jolieciteprogram/src/test/testDeva.java
@@ -0,0 +1,29 @@
+package test;
+import domain.valueobjects.*;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+class testDeva {
+    public static void main(String[] args) {
+
+        // test print date Concert
+        int[] date = new int[]{ 2021,12,2,23,00 };
+        Concert test = new Concert("concert test",date);
+        System.out.println(test.getDate());
+        System.out.printf("Name: %s\tDate: %s\n",test.getName(), test.getStrDateFormatted());
+        
+        // test print date TheaterPlay
+        // ArrayList<Calendar> arr = new ArrayList<Calendar>();
+        // Calendar c = new GregorianCalendar(2013,1,28,13,24,56);
+        // arr.add(c);
+        // arr.add(c);
+        // arr.add(c);
+        // arr.add(c);
+        // arr.add(c);
+
+        // TheaterPlay testTeatre = new TheaterPlay("tp",arr);
+        // testTeatre.getDateFormatted();
+    }
+    
+}
